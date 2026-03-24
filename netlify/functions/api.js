105b13d49839e7dc8320598457fcf6b999ea0d56
ArcticWabbit <arcticwabbit@gmail.com>
2026-03-24 00:05:00 -0400

    api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,23 +1,22 @@
 const fs = require("fs");
 const path = require("path");
 
-exports.handler = async function () {
+exports.handler = async () => {
   try {
-    const filePath = path.resolve(__dirname, "../../public/db.json");
+    const filePath = path.join(process.cwd(), "public", "db.json");
     const data = fs.readFileSync(filePath, "utf-8");
 
     return {
       statusCode: 200,
       headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
+        "Content-Type": "application/json"
       },
       body: data
     };
-  } catch (error) {
+  } catch (err) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Failed to load data" })
+      body: JSON.stringify({ error: err.message })
     };
   }
 };
