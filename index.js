109cbaacd6cab22d699afde1cf0c278011b51b22
ArcticWabbit <arcticwabbit@gmail.com>
2026-04-14 10:01:21 -0400

    MongoDB

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+// https://resilient-sundae-9b1292.netlify.app
+
 const http = require("http");
-const fs = require("fs");
 const path = require("path");
+const fs = require("fs");
+const { MongoClient } = require("mongodb");
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
+const uri = "mongodb+srv://mmahin_db_user:test123@cluster0.gftacxt.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+let db;
+
+async function connectDB() {
+  await client.connect();
+  db = client.db("test");
+}
+
+connectDB();
+
+const server = http.createServer(async (req, res) => {
 
   if (req.url === "/") {
     const filePath = path.join(__dirname, "public", "index.html");
@@ -15,11 +31,15 @@ const server = http.createServer((req, res) => {
   }
 
   else if (req.url === "/api") {
-    const filePath = path.join(__dirname, "public", "db.json");
-    const data = fs.readFileSync(filePath, "utf-8");
+    try {
+      const users = await db.collection("users").find().toArray();
 
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(data);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(users));
+    } catch (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Database error");
+    }
   }
 
   else {
--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,11 +1,27 @@
-const data = require("./db.json");
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb+srv://mmahin_db_user:test123@cluster0.gftacxt.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
 
 exports.handler = async () => {
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  };
+  try {
+    await client.connect();
+    const db = client.db("test");
+
+    const users = await db.collection("users").find().toArray();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(users)
+    };
+
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: "Database error"
+    };
+  }
 };
